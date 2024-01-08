93526e10eb15c4abc4d41e7fbaed1c1618e7f60c
esteban-silvera <estebansilvera000@gmail.com>
2024-01-08 00:04:17 -0300

    .\3-tasks.py

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -5,6 +5,7 @@ import asyncio
 
 wait_random = __import__("0-basic_async_syntax").wait_random
 
+
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """coments"""
     loop = asyncio.get_event_loop()
