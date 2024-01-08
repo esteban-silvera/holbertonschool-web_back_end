1db85d1128d089f771457c3e06b51a2bae2e3964
esteban-silvera <estebansilvera000@gmail.com>
2024-01-08 00:03:46 -0300

    .\3-tasks.py

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -3,7 +3,7 @@
 import asyncio
 
 
-from . import wait_random
+wait_random = __import__("0-basic_async_syntax").wait_random
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """coments"""
