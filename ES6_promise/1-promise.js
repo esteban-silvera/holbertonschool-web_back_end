f48a3793eb5ceb3dfb24341076d50f6ec31fa0c1
esteban-silvera <estebansilvera000@gmail.com>
2024-01-28 17:22:52 -0300

    .\1-promise.js

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,15 +1,14 @@
-// coment
 function getFullResponseFromAPI(success) {
     return new Promise((resolve, reject) => {
-      if (success) {
-        resolve({
-          status: 200,
-          body: 'Success',
-        });
-      } else {
-        reject(new Error('The fake API is not working currently'));
-      }
-    });
-  }
-  
-  export default getFullResponseFromAPI;
+        if (success) {
+            resolve({
+                status: 200,
+                body: 'Success',
+            });
+            } else {
+                reject(new Error('The fake API is not working currently'));
+                }
+                });
+}
+
+export default getFullResponseFromAPI;
