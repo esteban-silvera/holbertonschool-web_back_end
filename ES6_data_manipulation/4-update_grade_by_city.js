3288263068d6b1bd4f16d2296b1b15c6927fad56
esteban-silvera <estebansilvera000@gmail.com>
2024-02-04 20:32:49 -0300

    .\4-update_grade_by_city.js

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,15 +1,13 @@
 export default function updateStudentGradeByCity(students, city, grades) {
     return students
-    
-    .filter((student) => student.location === city)
-    .map((student) => {
+      .filter((student) => student.location === city)
+      .map((student) => {
         let grade = 'N/A';
         for (const g of grades) {
-            if (g.studentId === student.id) {
-                grade = g.grade;
-            }
+          if (g.studentId === student.id) {
+            grade = g.grade;
+          }
         }
-    return { ...student, grade };
-    });
-}
-
+        return { ...student, grade };
+      });
+  }
