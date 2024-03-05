51072a049af8e830844d0f243a2f76ca70a413c8
esteban-silvera <estebansilvera000@gmail.com>
2024-03-05 18:52:46 -0300

    try fix spaces

--- ES6_classes/1-main.js
+++ ES6_classes/1-main.js
@@ -1,9 +1,1 @@
 import initializeRooms from './1-make_classrooms.js';
-
-export default function initializeRooms() {
-    return [
-      new ClassRoom(19),
-      new ClassRoom(20),
-      new ClassRoom(34),
-    ];
-}
